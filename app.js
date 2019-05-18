7efa73fa757fe27458c4bd5490f7836e8be13534
Harry Sandhu <hrrsandhu6@gmail.com>
2019-05-18 15:24:35 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express")
+var path = require("path")
+var app = express()
+
+var server = require("http").Server(app)
+var io = require('socket.io')(server)
+var api = require('./api/api')
+var bodyParser = require("body-parser")
+var morganLogger = require("morgan")
+
+
+const PORT = 8220
+
+app.use(morganLogger('dev'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('json spaces', 2);
+
+server.listen(PORT, () =>{
+    console.log("listening on port :" , PORT)
+})
+
+
+app.use("/api", api)
+
