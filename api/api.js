9ec9379a40ab361f0adcaaee9e749449a28c8fda
Harry Sandhu <hrrsandhu6@gmail.com>
2019-05-20 00:55:41 -0700

    api database connection

--- api/api.js
+++ api/api.js
@@ -4,45 +4,30 @@ var api = express.Router()
 
 var mysql = require("mysql")
 
-// const HOST = "localhost"
-// const USER = "root"
-// const PASSWORD = "Harry1032."
-// const DB = "hasettDev"
+const HOST = "localhost"
+const USER = "root"
+const PASSWORD = "Harry1032."
+const DB = "hasettDev"
 
-// var conn = mysql.createConnection({
-//     host: HOST,
-//     user: USER,
-//     password: PASSWORD,
-//     database: DB
-// })
+var conn = mysql.createConnection({
+    host: HOST,
+    user: USER,
+    password: PASSWORD,
+    database: DB
+})
 
 
 
-// conn.connect((err) =>{
-//     console.log("Connection error!", err)
-// })
+conn.connect((err) =>{
+    console.log("Connection error!", err)
+})
+
+
 
 
 
 api.get("/users", (req, res) =>{
-    const users = {
-        1 : {
-            name : "harry",
-            email : "hrasa@fanm.com"
-        },
-        2 : {
-            name : "Mike",
-            email : "fmafa@fanm.com"
-        },
-        3 : {
-            name : "Steve",
-            email : "rdvs@fanm.com"
-        }
-        
-    }
 
-
-    res.json(users)
 })
 
 
