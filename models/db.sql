ef6097b376a7c127c11cc4695fca90aec5e77cdc
Harry Sandhu <hrrsandhu6@gmail.com>
2019-05-20 03:18:00 -0700

    bug fixes for db

--- models/db.sql
+++ models/db.sql
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS users(
     username VARCHAR (255) NOT NULL UNIQUE,
     profile_picture VARCHAR (255),
     header_picture VARCHAR (255),
-    signup_datetime DATETIME (255),
+    signup_datetime DATETIME NOT NULL,
     connections INT (11) DEFAULT 0,
     active_connections INT (11) DEFAULT 0,
     business_account INT (2) DEFAULT 1,
@@ -69,6 +69,7 @@ CREATE TABLE IF NOT EXISTS user_activities(
     user_activity_id INT AUTO_INCREMENT NOT NULL UNIQUE,
     u_id VARCHAR (100) NOT NULL,    
     activity_type_id INT (11) NOT NULL,
+    activity_datetime DATETIME,
     PRIMARY KEY (user_activity_id)
 );
 
@@ -78,7 +79,7 @@ CREATE TABLE IF NOT EXISTS user_activities(
 
 
 
-CREATE TABLE connections(
+CREATE TABLE IF NOT EXISTS connections(
     connection_count_id INT AUTO_INCREMENT NOT NULL UNIQUE,
     u_id VARCHAR (100) NOT NULL,
     connection_id VARCHAR (100) NOT NULL,
@@ -86,7 +87,7 @@ CREATE TABLE connections(
 );
 
 
-CREATE TABLE business_users(
+CREATE TABLE IF NOT EXISTS business_users(
     business_id VARCHAR (100) NOT NULL UNIQUE,
     u_id VARCHAR (100) NOT NULL,
     business_type_id INT (11) NOT NULL,
@@ -96,7 +97,7 @@ CREATE TABLE business_users(
     PRIMARY KEY(business_id)
 );
 
-CREATE TABLE business_keywords(
+CREATE TABLE IF NOT EXISTS business_keywords(
     keyword_id INT (11) AUTO INCREMENT
     business_id VARCHAR (100) NOT NULL,
     keyword VARCHAR (100) NOT NULL,
