5a5c6bfcfa560dbe8de5e21930efcd8783c1ae68
Harry Sandhu <hrrsandhu6@gmail.com>
2019-05-23 13:55:05 -0700

    Api

--- api/api.js
+++ api/api.js
@@ -1,13 +1,20 @@
 var express = require("express")
 var api = express.Router()
-
-
+var fs = require('fs')
+var jwt = require('jsonwebtoken')
+var crypto = require('crypto')
+var sha256 = require('js-sha256')
+var privateKey = fs.readFileSync('./security/private.key', 'utf8')
+var publicKey = fs.readFileSync('./security/public.key', 'utf8')
 var mysql = require("mysql")
 
-const HOST = "localhost"
-const USER = "root"
-const PASSWORD = "Harry1032."
+import {verifyAuthToken} from '../helperFunctions/auth'
+
+const HOST = "138.68.28.178"
+const USER = "dazzler"
+const PASSWORD = "@./%_1ab!dazzler" 
 const DB = "hasettDev"
+
 
 var conn = mysql.createConnection({
     host: HOST,
@@ -17,18 +24,63 @@ var conn = mysql.createConnection({
 })
 
 
-
 conn.connect((err) =>{
-    console.log("Connection error!", err)
+    if(err)
+        console.log("Connection error!", err)
+    else console.log("Connection successful")
 })
 
 
 
 
 
-api.get("/users", (req, res) =>{
+
+api.get("/login", (req, res) =>{
+    
+    const username = "harrysandhu"
+    const password = "9915081032"
+    const salt = crypto.randomBytes(20).toString('hex');
+    const passwordHash = sha256.hmac(salt, password)
+    const uid = 432
+    //put the shit in the database success and all that
+
+    const payload  = {
+        username : username, 
+        uid: uid,
+        loginTime : Date.now()
+    }
+
+    var signOptions =  {
+        subject: username,
+        algorithm:  "RS256" 
+    }
+    var token = jwt.sign(payload, privateKey, signOptions)
+    console.log("Generated Token: ", token)
+    return res.json({
+        status: 'LOGIN_SUCCESS',
+        token : token
+    })
+})  
+
+api.post("/post", verifyAuthToken, (req, res) =>{
+    //get the auth data from the token
+    jwt.verify(req.token, publicKey, (err, authData) =>{
+        if(err){
+            console.log(err)
+            res.json({error : 'Unauthorized.'})
+        }else {
+            console.log(authData)
+            res.json(authData)
+        }
+    })
+
 
 })
+
+
+
+
+
 
 
 
