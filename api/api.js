d5c0ed1e4fc673068f14621a1285fe7d5c938d9e
Harry Sandhu <hrrsandhu6@gmail.com>
2019-06-23 02:52:58 -0700

    Bug fixes

--- api/api.js
+++ api/api.js
@@ -9,6 +9,7 @@ var publicKey = fs.readFileSync('./security/public.key', 'utf8')
 var mysql = require("mysql")
 
 import {verifyAuthToken} from '../functions/helpers'
+import { RESPONSES, BASE_DEV } from "../functions/helperConstants";
 
 const HOST = "138.68.28.178"
 const USER = "dazzler"
@@ -19,7 +20,9 @@ import {
 	genRandomToken,
 	checkEmailAddress,
 	checkPassword,
-	executeQuery
+    checkUsername,
+	executeQuery,
+    jwtVerifyUser
 } from "../functions/helpers";
 
 import {
@@ -116,58 +119,66 @@ api.post("/post", verifyAuthToken, (req, res) =>{
 })
 
 
-api.post("/user_data", verifyAuthToken, async function (req, res) {
-    jwt.verify(req.token, publicKey, async (err, currentUser) =>{
-        if(err) res.json(RESPONSES.USER_UNAUTHORIZED)
-        let { auth_id, 
-            u_id, 
+api.post("/user_data", verifyAuthToken, async (req, res) =>{
+    try{
+        //verify jwt token and get the currentUser
+        let currentUser = await jwtVerifyUser(req.token, publicKey)
+
+        //destructure currentUser
+        let {
+            auth_id,
+            u_id,
             email_address
-            } = currentUser
-
+        } = currentUser
         
-        //get userData
+        //Check if the currentUser has 'userData'. 
+        //userData would have a contract of the fields below.
         if(!req.body.hasOwnProperty('userData')){
-            return res.json(RESPONSES.INVALID_REQUEST)
+            throw RESPONSES.INVALID_REQUEST
         }
-
-        try{
-            let {
-                nameTF,
+        //userData needs to have the following fields.
+        let {
+            nameTF,
             usernameTF,
             userDescriptionTF,
             profilePictureImageData,
             profilePictureIsSelected
-            } = req.body.userData
-
-
-            //convert profilePictureImageData into jpg file and get path name
-            let cryptFilename = crypto.randomBytes(10).toString('hex')
+        } = req.body.userData
+        
+        // 
+        let profilePictureURI = BASE_DEV + "/defaultUser/profile/" + 'defaultProfilePicture.jpg' 
+        let cryptFilename = 'defaultProfilePicture.jpg'
+        if(profilePictureIsSelected){
+             cryptFilename = crypto.randomBytes(10).toString('hex')
 
             let profilePictureURI_RESPONSE = await saveImage(u_id, cryptFilename, profilePictureImageData)
 
+            console.log(profilePictureURI_RESPONSE)
             if(profilePictureURI_RESPONSE.errorStatus){
                 return profilePictureURI_RESPONSE
+            }else{
+                profilePictureURI = profilePictureURI_RESPONSE.imageURI
             }
 
-            let checkUsername_RESPONSE = await checkUsername(username)
+        }
+
+        let checkUsername_RESPONSE = await checkUsername(usernameTF, u_id)
 
             //---------LOG---------
 
-            if(!checkEmailAddress_RESPONSE.usernameStatus){
-                return RESPONSES.USERNAME_UNAVAILABLE
-            }
+        if(!checkUsername_RESPONSE.usernameStatus){
+                throw checkUsername_RESPONSE
+        }
 
-
-            let authSQL = "INSERT INTO auth SET ? WHERE ?? = ?"
-            let usernameInsert = {username}
-            let authInserts = [usernameInsert, "auth_id", auth_id]
-            
-            authSQL = mysql.format(authSQL, authInserts)
-
-            let authQueryResponse = await executeQuery(authSQL)
-            
-            let userSQL = "INSERT INTO users SET ? WHERE ?? = ?"
-            let userDataInserts = {
+        let authSQL = "UPDATE auth SET ? WHERE auth_id = ?"
+            let usernameInsert = {username: usernameTF}
+            let authInserts = [usernameInsert, auth_id]
+        authSQL = mysql.format(authSQL, authInserts)
+        console.log(authSQL)
+        let authQueryResponse = await executeQuery(authSQL)
+        console.log(authQueryResponse)
+        let userSQL = "UPDATE users SET ? WHERE ?? = ?"
+         let userDataInserts = {
                 name: nameTF,
                 username: usernameTF,
                 userDescription: userDescriptionTF,
@@ -177,23 +188,104 @@ api.post("/user_data", verifyAuthToken, async function (req, res) {
             let userInserts = [userDataInserts, 'u_id', u_id]
             userSQL = mysql.format(userSQL, userInserts)
 
-            let userDataQueryResponse = await executeQuery(userSQL)
 
+            let userDataQueryResponse = await executeQuery(userSQL)
             return res.json(RESPONSES.SUCCESS)
+
+
+    }catch(e){
+        console.log(e)
+        return res.json(e)
+    }
+    
+})
+
+
+
+
+
+
+
+// api.post("/user_data", verifyAuthToken, async function (req, res) {
+//     jwt.verify(req.token, publicKey, async (err, currentUser) =>{
+//         if(err) res.json(RESPONSES.USER_UNAUTHORIZED)
+//         let { auth_id, 
+//             u_id, 
+//             email_address
+//             } = currentUser
+
+        
+//         //get userData
+//         if(!req.body.hasOwnProperty('userData')){
+//             return res.json(RESPONSES.INVALID_REQUEST)
+//         }
+
+//         try{
+//             let {
+//                 nameTF,
+//             usernameTF,
+//             userDescriptionTF,
+//             profilePictureImageData,
+//             profilePictureIsSelected
+//             } = req.body.userData
+
+
+//             if(profilePictureIsSelected){
+//                 //convert profilePictureImageData into jpg file and get path name
+//                 let cryptFilename = crypto.randomBytes(10).toString('hex')
+
+//                 let profilePictureURI_RESPONSE = await saveImage(u_id, cryptFilename, profilePictureImageData)
+
+//                 if(profilePictureURI_RESPONSE.errorStatus){
+//                     return profilePictureURI_RESPONSE
+//                 }
+//             }
+
+//             let checkUsername_RESPONSE = await checkUsername(username)
+
+//             //---------LOG---------
+
+//             if(!checkEmailAddress_RESPONSE.usernameStatus){
+//                 return RESPONSES.USERNAME_UNAVAILABLE
+//             }
+
+
+//             let authSQL = "INSERT INTO auth SET ? WHERE ?? = ?"
+//             let usernameInsert = {username}
+//             let authInserts = [usernameInsert, "auth_id", auth_id]
+            
+//             authSQL = mysql.format(authSQL, authInserts)
+
+//             let authQueryResponse = await executeQuery(authSQL)
+            
+//             let userSQL = "INSERT INTO users SET ? WHERE ?? = ?"
+//             let userDataInserts = {
+//                 name: nameTF,
+//                 username: usernameTF,
+//                 userDescription: userDescriptionTF,
+//                 profilePictureURI: profilePictureURI,
+//                 profilePictureFilename: cryptFilename
+//             }
+//             let userInserts = [userDataInserts, 'u_id', u_id]
+//             userSQL = mysql.format(userSQL, userInserts)
+
+//             let userDataQueryResponse = await executeQuery(userSQL)
+
+//             return res.json(RESPONSES.SUCCESS)
 
             
 
-        }catch(e){
-            return res.json(RESPONSES.ERR_SYSTEM)
-        }
+//         }catch(e){
+//             return res.json(RESPONSES.ERR_SYSTEM)
+//         }
 
 
-        //update userData - user
-        //update username - auth
+//         //update userData - user
+//         //update username - auth
         
         
-    })
-})
+//     })
+// })
 
 
 
