b4066281382fada6edb72a1bfa2d609f7f3b0431
Harry Sandhu <hrrsandhu6@gmail.com>
2019-10-06 17:14:27 -0700

    Interfaces - typescript

---
+++ core/Interfaces/DBResult.ts
@@ -0,0 +1,3 @@
+export default interface DBResult<T> {
+    data: T[]
+}
---
+++ core/Interfaces/UserInterface.ts
@@ -0,0 +1,4 @@
+
+export default interface UserInterface{
+    // /static jwtVerifyUser(reqToken:string, publicKey:string): any;
+}
